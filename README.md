110658a053f21d2e22f6ba056462576f7fc8ee26
alexlin2 <alex.lin416@outlook.com>
2022-03-17 18:54:45 -0700

    working code and markdown

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Monocular_VO
+## Abstract
+
+Determining the trajectory or direction of travel of a mobile robot or car is essential for navigation and and movement. If you have ever driven a car then you would notice a number on your dashboard that tells you how far your car has travel in it's lifespan. This is called an odometer. This information is essential for mechanics to diagnose the health and deterioration of your vehicle and it determines your car's used value on the second hand market. Odometry is also vital for robot navigation, this information is used to determine the state of the robot at any given time. However, odometry is often hard to do without supporting hardware, and inaccurate when using a traditional wheel speed sensor. This project aims to do simple odometry using just a single rgb camera. It solves a complex problem using just software and knowledge of computer vision. 
+
+# Problem statement
+
+Odometry in essense is determining the distance traveled and the direction of travel of the camera at any given time. So for every time instance $t$, we want to determine the pose vector $[x^{t} y^{t} z^{t} \alpha^{t} \beta^{t} \gamma^{t}]$ that describes the position and orientation of the camera. Note that $\alpha^{t}, \beta^{t}, \gamma^{t}$ is in Euler angles, and $x^{t}, y^{t}, z^{t} $ is in Cartesian coordinates. For this problem, we are given the initial known position, and the initial orientation of the camera. They are given as a 3 by 3 rotation matrix $R_{pos}$ and a 3 by 1 translation vector $t_{pos}$. Camera instrinsic such as focal length and the principle point will also be known, and we are assuming a pinhole camera model. 
+
+# Algorithm
+
+We will use Nister's Five Point Algorithm for Essential Matrix estimation. The essential matrix is a 3x3 matrix that relates corresponding points in two images, it is similar to the homography matrix that we learned in class. We will also use the FAST(Features from accelerated segment test) algorithm to detect corners and features in the image, it is similar to the ORB features that we learned in class. 
+
+Here is an outline of the algorithm:
+
+1. Capture two consecutive images at $t$ and $t+1$
+2. Use `cv2.FastFeatureDetector` to detect keypoints in the first image at time $t$
+3. Use LK optical flow `cv2.calcOpticalFlowPyrLK` to determine the corresponding keypoints in the second image at time $t+1$ 
+4. Use Nister's Five Point Algorithm `cv2.findEssentialMat` with RANSAC to find the essential matrix that describe the corresponding point from image $t$ to image $t+1$
+5. Estimate the rotation matrix $R$ and the translation vector $t$ from the essential matrix that was obtained in the last step.
+6. Use the formula $R_{pos}^{t+1} = RR_{pos}^{t}$ and $t_{pos}^{t+1} = t_{pos}^{t} + tR_{pos}$ to determine the new pose vector that describes the position and orientation of the camera
+
+# Dataset
+
+The dataset used is [KITTI Visual Odometry](http://www.cvlibs.net/datasets/kitti/eval_odometry.php) 
--- vo.py
+++ vo.py
@@ -1,1 +1,154 @@
 import cv2
+import numpy as np 
+import os
+class MonocularVO():
+    
+    def __init__(self, focal, pp, detector, lk_params, pose_file_path, start_id):
+        self.focal = focal
+        self.pp = pp
+        self.detector = detector
+        self.lk_params = lk_params
+        self.features_img = None
+        self.lk_img = None
+        self.frame_id = start_id
+        self.start_id = start_id
+        self.min_features = 0
+        self.R = np.zeros(shape=(3, 3), dtype=np.float32)
+        self.t = np.zeros(shape=(3, 1), dtype=np.float32)
+        with open(pose_file_path) as f:
+            self.pose = f.readlines()
+
+        self.get_initial_pose()
+        print(self.t)
+
+    def get_initial_pose(self):
+        start_pose = self.pose[self.start_id].strip().split()
+        x = float(start_pose[3])
+        y = float(start_pose[7])
+        z = float(start_pose[11])
+        self.t[0] = -x
+        self.t[1] = -y
+        self.t[2] = -z
+        self.R[0, :] = start_pose[:3]
+        self.R[1, :] = start_pose[4:7]
+        self.R[2, :] = start_pose[8:11]
+
+    def detect_features(self, frame):
+
+        kp = self.detector.detect(frame)
+        cv2.drawKeypoints(self.features_img, kp, None, color=255)
+        return np.array([x.pt for x in kp], dtype=np.float32).reshape(-1, 1, 2)
+
+    def lk_optical_flow(self, p0, old_frame, current_frame):
+
+        self.p1, st, _ = cv2.calcOpticalFlowPyrLK(old_frame, current_frame, self.p0, None, **self.lk_params)
+
+        self.good_old = self.p0[st == 1].reshape(-1, 1, 2)
+        self.good_new = self.p1[st == 1].reshape(-1, 1, 2)
+
+        mask = np.zeros_like(old_frame)
+
+        for i, (new, old) in enumerate(zip(self.good_new, self.good_old)):
+            a, b = new.ravel()
+            c, d = old.ravel()
+            mask = cv2.line(mask, (int(a), int(b)), (int(c), int(d)), 255, 2)
+            lk_img = cv2.circle(old_frame, (int(a), int(b)), 5, 255, -1)
+        self.lk_img = cv2.add(lk_img, mask)
+        
+    def get_absolute_scale(self):
+
+        pose = self.pose[self.frame_id - 1].strip().split()
+        x_prev = float(pose[3])
+        y_prev = float(pose[7])
+        z_prev = float(pose[11])
+        pose = self.pose[self.frame_id].strip().split()
+        x = float(pose[3])
+        y = float(pose[7])
+        z = float(pose[11])
+
+        true_vect = np.array([[x], [y], [z]])
+        prev_vect = np.array([[x_prev], [y_prev], [z_prev]])
+
+        self.true_coord = true_vect
+        
+        return np.linalg.norm(true_vect - prev_vect)
+
+    def process_frame(self, old_frame, current_frame):
+
+        self.features_img = current_frame.copy()
+        self.lk_img = current_frame.copy()
+        
+        if self.min_features < 2000:
+            self.p0 = self.detect_features(old_frame)
+
+        
+        self.lk_optical_flow(self.p0, old_frame, current_frame)
+
+        E, _ = cv2.findEssentialMat(self.good_new, self.good_old, self.focal, self.pp, cv2.RANSAC, 0.999, 1.0, None)
+
+        absolute_scale = self.get_absolute_scale()
+
+  
+        _, R, t, _ = cv2.recoverPose(E, self.good_old, self.good_new, focal=self.focal, pp=self.pp)
+        
+        if (absolute_scale > 0.1 and absolute_scale < 10):
+            print(absolute_scale)
+            self.t = self.t + absolute_scale * self.R.dot(t)
+            self.R = R.dot(self.R)
+
+        self.p0 = self.good_new.reshape(-1, 1, 2)
+
+    def get_predicted_coords(self):
+        diag = np.array([[-1, 0, 0],
+                        [0, -1, 0],
+                        [0, 0, -1]])
+        adj_coord = np.matmul(diag, self.t)
+
+        return adj_coord.flatten()
+
+    def get_true_coords(self):
+        return self.true_coord.flatten()
+
+if __name__ == '__main__':
+
+    dataset_img_path = '/home/alexlin/Developer/Monocular_VO/dataset/images/00/image_0/'
+    dataset_pose_path = '/home/alexlin/Developer/Monocular_VO/dataset/poses/00.txt'
+    
+    focal = 718.8560
+    pp = (607.1928, 185.2157)
+
+    lk_params=dict(winSize  = (21,21), criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 30, 0.01))
+
+    fast_detector=cv2.FastFeatureDetector_create(threshold=25, nonmaxSuppression=True)
+
+    vo = MonocularVO(focal, pp, fast_detector, lk_params, dataset_pose_path, 0)
+
+    traj = np.zeros(shape=(600, 800, 3))
+
+    while(vo.frame_id < len(os.listdir(dataset_img_path))):
+        old_frame = cv2.imread(dataset_img_path + str(vo.frame_id).zfill(6)+'.png', 0)
+        new_frame = cv2.imread(dataset_img_path + str(vo.frame_id+1).zfill(6)+'.png', 0)
+
+        if(old_frame is not None and new_frame is not None):
+            cv2.imshow('frame', new_frame)
+            vo.process_frame(old_frame, new_frame)
+
+            cv2.imshow('lk_frame', vo.lk_img)
+            cv2.imshow('kp_frame', vo.features_img)
+
+            coord = vo.get_predicted_coords()
+            true_coord = vo.get_true_coords()
+            draw_x, draw_y, draw_z = [int(round(x)) for x in coord]
+            true_x, true_y, true_z = [int(round(x)) for x in true_coord]
+            traj = cv2.circle(traj, (true_x + 400, true_z + 300), 1, list((0, 0, 255)), 4)
+            traj = cv2.circle(traj, (draw_x + 400, draw_z + 300), 1, list((0, 255, 0)), 4)
+            
+            cv2.imshow('trajectory', traj)
+
+            k = cv2.waitKey(30) & 0xff
+            if k == 27:
+                break
+
+        vo.frame_id += 1
+
+    cv2.destroyAllWindows()
